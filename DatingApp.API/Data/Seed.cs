baecaa4a13e21cf7c990cef53f7f4042117a45fc
Sepehr Shahsavar <sepehrshahsavar14@gmail.com>
2020-08-09 18:32:38 +0430

    HomePageCompeleted

---
+++ DatingApp.API/Data/Seed.cs
@@ -0,0 +1,42 @@
+﻿using DatingApp.API.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatingApp.API.Data
+{
+    public static class Seed
+    {
+        public static void SeedUsers(DataContext context)
+        {
+            if (!context.Users.Any())
+            {
+                var usersData = System.IO.File.ReadAllText("Data/UserSeedData.json");
+                var users = JsonConvert.DeserializeObject<List<User>>(usersData);
+                
+                foreach(var user in users)
+                {
+                    byte[] passwordHash, passwordSalt;
+                    CreatePasswordHash("password" , out passwordHash , out passwordSalt);
+
+                    user.PasswordHash = passwordHash;
+                    user.PasswordSalt = passwordSalt;
+                    user.Username = user.Username.ToLower();
+                    context.Add(user);
+                }
+                context.SaveChanges();
+            }
+        }
+
+        private static void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passordSalt)
+        {
+            using (var hmac = new System.Security.Cryptography.HMACSHA512())
+            {
+                passordSalt = hmac.Key;
+                passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            }
+        }
+    }
+}
